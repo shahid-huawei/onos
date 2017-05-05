9539dfa4daabb18314f178196cea87c47e8852f5
Mohammad Shahid <mohammad.shahid1@huawei.com>
2017-05-05 10:59:12 +0530

    Contrail Demo exception fix

--- protocols/bgp/bgpio/src/main/java/org/onosproject/bgpio/protocol/evpn/BgpEvpnNlriImpl.java
+++ protocols/bgp/bgpio/src/main/java/org/onosproject/bgpio/protocol/evpn/BgpEvpnNlriImpl.java
@@ -99,7 +99,8 @@ public class BgpEvpnNlriImpl implements BgpEvpnNlri {
                 routeNlri = BgpEvpnRouteType2Nlri.read(tempCb);
                 break;
             default:
-                break;
+                throw new BgpParseException(BgpErrorType.UPDATE_MESSAGE_ERROR, BgpErrorType.MISSING_WELLKNOWN_ATTRIBUTE, null);
+                    //break;
             }
             return new BgpEvpnNlriImpl(type, routeNlri);
         } else {
